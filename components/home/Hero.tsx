4f3f637f0c8c89db6c44bbbba2dccc35c9a5f44a
Vivek Sharma <viveksharmaa6307016893@gmail.com>
2026-06-05 23:44:46 +0530

    feat(ui): add hero ui

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Star } from "lucide-react"
 
 const Hero = () => {
   return (
-    <div className="relative ounded-2xl overflow-hidden bg-amber-200 aspect-auto">
+    <div className="relative rounded-2xl overflow-hidden  aspect-auto">
       <div className="bg-green-400 w-full h-full">
-        <img src="/images/home/banner.png" alt="" className="w-full h-full object-cover" />
+        <img src="/images/home/banner.png" alt="" className="w-full h-full object-cover hover:brightness-105 transition duration-300 ease-in-out" />
       </div>
-      <div></div>
+      <div>
+        <div>
+          <p></p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
