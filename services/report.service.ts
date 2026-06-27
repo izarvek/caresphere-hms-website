f33e274febfc889b5771372dbed2e089f6dea70e
Vivek Sharma <viveksharmaa6307016893@gmail.com>
2026-06-28 00:55:35 +0530

    fix: enhance report.service.ts file

--- services/report.service.ts
+++ services/report.service.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
 export const getPatientReports = async (patientId: string) => {
-  return await prisma.medical_record.findMany({
+  return await prisma.medicalRecord.findMany({
     where: { patientId },
   });
 };
